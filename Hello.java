44958b68ca9867af955201a630aee0b2122bf3ed
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-01-07 15:19:21 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class main{
 	public static void main(String [] args){
 		System.out.println("hello");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt():
+		int sum = a + b ;
+		System.out.println(sum);
+		
 	}
 }
